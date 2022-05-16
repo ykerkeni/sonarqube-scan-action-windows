efbba948bf1a63062e49669233e412f8ffc52f88
ykerkeni <103499104+ykerkeni@users.noreply.github.com>
2022-05-16 13:41:33 +0200

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,2 +1,8 @@
-param ($input1, $input2)
-Write-Host "$input1 $input2"
+param ($url, $projectkey, $projectname, $api_token)
+
+Write-Host "$url"
+Write-Host "$projectkey"
+Write-Host "$projectname"
+Write-Host "$api_token"
+
+& "D:\APP\SonarScanner\SonarScanner.MSBuild.exe" begin /k:$projectkey "/n:$projectname" /v: /d:sonar.host.url=$url /d:sonar.login=$api_token
